17176a80794f6df44f1d721b751bd55d552b2e6e
rithikrajkumar4 <rithikrajkumar@gmail.com>
2023-03-01 00:31:19 +0530

    intial commit

---
+++ backend/config/database.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const connnectDatabase = ()=>{
+    mongoose.set('strictQuery', true);
+    mongoose.connect(process.env.DB_URI,{useNewUrlParser:true,useUnifiedTopology:true}).then((data)=>{
+        console.log(`Mongodb connected with server:${data.connection.host}`);
+    }); 
+}
+
+module.exports = connnectDatabase;
