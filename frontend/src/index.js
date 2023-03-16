98919ea87989a7c1e572d4d52fec9154ee86da2a
Aj-108 <architjain1082002@gmail.com>
2023-03-16 22:37:21 +0530

    avoid

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -23,7 +23,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <AlertProvider template={AlertTemplate} {...options}>
+      <AlertProvider template={() => AlertTemplate} {...options}>
         <App />
       </AlertProvider>
     </Provider>
