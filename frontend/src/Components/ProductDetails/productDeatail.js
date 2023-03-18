ac037035236413940aee9552d64803904fc31ede
Aj-108 <architjain1082002@gmail.com>
2023-03-19 02:55:26 +0530

    product done

--- frontend/src/Components/ProductDetails/productDeatail.js
+++ frontend/src/Components/ProductDetails/productDeatail.js
@@ -40,7 +40,7 @@ const ProductDetail = () => {
   },[]);
 
   return (
-    <div>
+    <div className="productDetails">
       <Navbar />
       <div className="logo__" onClick={() => navigate('/')}>
         <Logo  className='logo product__logo'/>
@@ -52,7 +52,7 @@ const ProductDetail = () => {
         <loader/>
       ):(
 
-        <div className="container-lg productDetails">
+        <div className="container-lg ">
         <div className="row">
           <div className="col-lg-6" align='center'>
             {/* <Zoom img src={img}  className="product__image"/> */}
