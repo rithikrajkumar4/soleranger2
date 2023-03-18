f32d957710a7b54d3c121b021c0179759739c2b6
rithikrajkumar4 <rithikrajkumar@gmail.com>
2023-03-18 19:33:38 +0530

    some

--- frontend/src/Components/ProductDetails/productDeatail.js
+++ frontend/src/Components/ProductDetails/productDeatail.js
@@ -42,24 +42,6 @@ const ProductDetail = () => {
           </div>
         </div>
       </div>
-=======
-import React, { Fragment } from "react";
-import Footer from "../Footer/Footer";
-import Navbar from "../modals/Navbar/Navbar";
-
-
-
-const ProductDetail = () => {
-  return (
-    <div>
-      <Navbar />
-      <Fragment>
-        <div>ProductDetail</div>
-      </Fragment>
-
-      <Footer />
     </div>
-  );
-};
-
+  )};
 export default ProductDetail;
